637ce78e69209a4c84ce53809dcd7e87804b11df
mchafulumira <125554742+mc265@users.noreply.github.com>
2023-07-07 17:34:38 +0200

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -3,8 +3,16 @@ Highcharts.chart('container', {
         type: 'timeline',
         inverted: true
     },
+    colors: [
+    '#4185F3',
+    '#427CDD',
+    '#406AB2',
+    '#3E5A8E',
+    '#3B4A68',
+    '#363C46'
+  ],
     title: {
-        text: 'Timeline of Forest Products Statistics history'
+        text: 'Timeline of FAO Forest Products Statistics'
     },
     subtitle: {
         text: 'Info source: <a href="https://www.fao.org/forestry/statistics/80577/en/">forest product statistics</a>'
@@ -23,18 +31,9 @@ Highcharts.chart('container', {
         visible: false
     },
     series: [{
-        data: [{
-            name: 'FAO founded',
-            label: '1945: <a href="https://www.fao.org/home/en">16 October FAO</a>',
-            description: ' <img src="https://upload.wikimedia.org/wikipedia/commons/d/db/FAO_logo.svg" alt="test"/> '
-        }, {
-            name: 'UNECE timber committee',
-            label: '1946:<a href="https://www.fao.org/3/v6585e/v6585e11.htm#the">The evolution of forestry statistics</a> ',
-            description: ' <img src="https://www.fao.org/3/v6585e/V6585e33.jpg" alt="test"/>'
-       
-        }, {
+        data: [ {
             name: 'Yearbook publication',
-            label: '1950:<a href="https://www.fao.org/3/am444b/am444b.pdf">first ever year</a>',
+            label: '1947:<a href="https://www.fao.org/forestry/statistics/80570/en/">first ever yearbook</a>',
             description: '<img src="https://www.fao.org/3/AM447B/am447b00.jpg" alt="test"/>'
         }, {
             name: 'European timber statistics:1913_1950',
@@ -42,34 +41,35 @@ Highcharts.chart('container', {
             description: '<img src="https://unece.org/sites/default/files/styles/max_2600x2600/public/datastore/migrated_files/publications/DAM/timber/docs/etts/European_Timber_Statistics_1913-1950-cover.JPG?itok=T-KaXAWp" alt="test"/>'},
             {
             name: 'Pulp and paper publication',
-            label: '1968:First publication',
+            label: '1968:<a href="https://www.fao.org/forestry/statistics/80571/en/">first publication</a>',
             description: '<img src="https://m.media-amazon.com/images/I/41W5AMZjQjL._SX350_BO1,204,203,200_.jpg" alt="test"/>'
-        }, {
-            name: 'Recovered paper data',
-            label: '1970:included in forest products',
-            description: '<img src="https://www.recyclingtoday.com/remote/aHR0cHM6Ly9jZG4uZ2llLm5ldC9maWxldXBsb2Fkcy9pbWFnZS8yMDIyLzExLzIyL2Fkb2Jlc3RvY2sxMTk0NTYwMDcuanBn.EIslEgTh8M8.jpg?w=948&h=533&mode=pad&anchor=middlecenter&scale=both&bgcolor=F0F1F2"alt="test"/>'},
+        }, 
                 {
-            name: 'Classifications of forest products',
+            name: 'Classification of forest products',
             label: '1982:<a href="https://data.apps.fao.org/catalog/dataset/classification-of-forest-products">first ever classified</a>',
             description: '<img src="https://m.media-amazon.com/images/I/41k2jtHb06L._SX350_BO1,204,203,200_.jpg" alt="test"/>'},
                 {
             name: 'Capacity building on data collection',
-            label: '1984:the beginning of data',
+            label: '1984:wider reach of data',
             description: '<img src="https://www.fao.org/uploads/pics/Data_collection.jpg" alt="test"/>'},
  
             {
             name: 'Intersecretariat Working Group(IWG) on forest products formed',
             label: '1994: <a href="https://www.fao.org/forestry/statistics/iwg/en/">forest product statistics</a>',
-            description: '<img src="https://www.fao.org/forestry/36245-093a2c444d82f187de60e357bf2827928.jpg" alt="test"/>'},   
+            description: '<img src="https://www.fao.org/forestry/36245-093a2c444d82f187de60e357bf2827928.jpg" alt="test"/>'
+           
+},   
                  {
             name: 'Cross-laminated timber introduced',
             label: 'Early 1990s:new forest product',
                     description: '<img src="https://cascadebusnews.com/wp-content/uploads/2022/07/Morrison-Maierle.jpg" alt="test"/>'
                  },
-           {
-            name: 'Forestry trade flows data collection began',
-            label: '1997: <a href="https://www.fao.org/faostat/en/#data/FT">forestry trade flows</a>',
-            description: '<img src="https://www.globalwoodmarketsinfo.com/wp-content/uploads/2017/01/loading_forestry_1.jpg" alt="test"/>'},
+                         {
+            name: 'FAOSTAT forestry database',
+            label: '1995:database for access',
+                    description: ''
+                 },
+          
                 {    name: 'Launch of JFSQ',
             label: '1997: <a href="https://unece.org/forests/jfsq">Joint Forest Sector Questionnaire</a>',
             description: ''},
